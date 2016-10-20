5bfb40faea796626d975ca6f2eec5945c8fee19f
virgi680 <virgi680@gmail.com>
2016-10-20 10:38:35 +0200

    controller,crearEvento,registro-FRONT

--- FRONT/src/controller.js
+++ FRONT/src/controller.js
@@ -3,33 +3,68 @@
  */
 'use strict';
 angular.module('app', [])
-.controller('controlador', ['$scope', '$http', function($scope, $http) {
-	$scope.newPage = function() {
-		open('crearEvento.html','top=300,left=300,width=300,height=300');
-	}
-	
-	$scope.insert = function() {
-		//var popup = window.open("");
-		$scope.insertarEv = {
-				dia : $scope.dia,
-				hora: $scope.hora,
-				lugar: $scope.lugar,
-				nombre: $scope.nombre
-		};
-		//popup.document.write($scope.insertarEv);
-	
-			$http.post('http://localhost:8080/funme/crearEvento', $scope.lugar).success(function(data) {
-				alert("EXITO " + $scope.lugar);
-			}).error(function(data) {
-				alert("ERROR " + $scope.lugar);
-			});
+.controller('controlador', ['$scope','$http', function($scope, $http) {
+					$scope.newPage = function() {
+						open('crearEvento.html',
+								'top=300,left=300,width=300,height=300');
+					}
 
-//			$http.get('http://rest-service.guides.spring.io/greeting')
-//			.success(function(data) {
-//				alert(data.content);
-//			}).error(function(data) {
-//				alert("ERROR");
-//			});
+					$scope.insert = function() {
 
-	}
-} ]);
+						var insertarEv = {
+							dia : $scope.dia,
+							hora : $scope.hora,
+							lugar : $scope.lugar,
+							nombre : $scope.nombre
+						};
+
+						/*$http.post('http://localhost:8080/funme/crearEvento?insertarEv='+ insertarEv)
+						.success(function(data) {
+							alert("EXITO");
+						}).error(function(data) {
+							alert("ERROR");
+						});*/
+
+						
+						$http.post('http://localhost:8080/funme/crearEvento?dia='+$scope.dia+'&hora='+$scope.hora+'&lugar='+$scope.lugar+'&nombre='+$scope.nombre)
+						.success(function(data) {
+						 alert("EXITO");
+						}).error(function(data) {
+						 alert("ERROR");
+						});
+						
+
+						/*
+						 * $http.get('http://rest-service.guides.spring.io/greeting')
+						 * 
+						 * .success(function(data) {
+						 * 
+						 * alert(2data.content);
+						 * 
+						 * }).error(function(data) {
+						 * 
+						 * alert("ERROR");
+						 * 
+						 * });
+						 */
+
+					}
+					$scope.guardarRegistro = function() {
+						var registro = {
+								email : $scope.email,
+								password : $scope.password,
+								rpassword : $scope.rpassword,
+								nombre : $scope.nombre,
+								apellidos : $scope.apellidos,
+								fecha : $scope.fecha,
+								genero : $scope.genero
+							};
+						$http.post('http://localhost:8080/funme/guardarRegistro?email='+$scope.email+'&password='+$scope.password+'&rpassword='+$scope.rpassword+'&nombre='+$scope.nombre+'&apellidos='+$scope.apellidos+'&fecha='+$scope.fecha+'&genero='+$scope.genero)
+						.success(function(data) {
+						 alert("EXITO");
+						}).error(function(data) {
+						 alert("ERROR");
+						});
+					}
+					
+				} ]);
